95c9545e539d6488f2d01042aa88e611fa39b3bc
farrasramadhan <farrasramadhan@192.168.11.94>
2016-11-18 09:21:41 +0700

    DoorLock class & WindowBlind class

---
+++ FJHomeAutomation/src/actors/DoorLock.java
@@ -0,0 +1,19 @@
+package actors;
+
+public class DoorLock {
+	
+	private boolean isOn;
+	
+	public DoorLock() {
+		
+	}
+
+	public boolean isOn() {
+		return isOn;
+	}
+
+	public void setOn(boolean isOn) {
+		this.isOn = isOn;
+	}
+	
+}
